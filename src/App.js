05c8480278a9871309d0036e5d220bc2223f1e27
JavierPerezCaro <javierperezcaro10@gmail.com>
2025-09-25 20:11:22 -0300

    modificacion

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/home/Home';
 import Contacto from './Pages/contacto/Contacto';
-import Gallery from './Pages/gallery/Gallery'
+import Gallery from './Pages/gallery/Gallery';
 import About from './Pages/about/About';
 import Navbar from './Components/navbar/Navbar';
-import Footer from '.Components'
+import Footer from './Components/navbar/footer/Footer';
+
+function App() {
   return (
     <div className="App">
-<BrowserRouter>
-<Navbar/>
-<Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/gallery" element={<Gallery />} />
-  <Route path="/contacto" element={<Contacto />} />
-  <Route path="/about" element={<About />} />  
-</Routes>
-<Footer/>
-</BrowserRouter>
-
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/about" element={<About />} />  
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
